4e75e60f4b9ead9ac76d5532ae0aebc7030ded0c
Abu Sayeed Roni <roni@dal.ca>
2024-08-30 12:24:27 -0300

    Add exceptions in Decoder

--- src/image-decoder.cpp
+++ src/image-decoder.cpp
@@ -1,3 +1,5 @@
+#include <stdexcept>
+
 #include "image-decoder.h"
 #include "stb_image.h"
 #include "image.h"
@@ -8,6 +10,9 @@ Image ImageDecoder::decodeImage(const std::string& filepath) const {
     int32_t channels;
 
     uint8_t* buffer = stbi_load(filepath.c_str(), &width, &height, &channels, 0);
+    if (! buffer) {
+        throw std::runtime_error(std::string("Failed to decode image at ") + filepath);
+    }
     
     return Image(buffer, width, height, channels, [](void* data) {
         stbi_image_free(data); 
